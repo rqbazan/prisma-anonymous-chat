0de3ec8ced5129fee531441c583a65322c2c0402
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2020-01-10 21:18:14 -0500

    fix: read port from env vars

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import { parse } from 'url'
 import next from 'next'
 
 const dev = process.env.NODE_ENV !== 'production'
+const port = parseInt(process.env.PORT, 10) || 3000
 const app = next({ dev, dir: 'web' })
 const handle = app.getRequestHandler()
 
@@ -10,8 +11,8 @@ app.prepare().then(() => {
   createServer((req, res) => {
     const parsedUrl = parse(req.url, true)
     handle(req, res, parsedUrl)
-  }).listen(3000, err => {
+  }).listen(port, err => {
     if (err) throw err
-    console.log('> Ready on http://localhost:3000')
+    console.log(`> Ready on http://localhost:${port}`)
   })
 })
