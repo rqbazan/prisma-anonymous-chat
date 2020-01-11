23121ee59a178c61e77e0387b0bb9786d65ffde5
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2020-01-11 15:41:41 -0500

    refactor: change ext to .gql

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
 import next from 'next'
 import { GraphQLServer } from 'graphql-yoga'
-import resolvers from './graphql'
-import router from './routes'
+import resolvers from './resolvers'
+import routes from './routes'
 import { prisma } from '../generated/prisma-client'
 
 const dev = process.env.NODE_ENV !== 'production'
@@ -10,11 +10,11 @@ const app = next({ dev, dir: 'web' })
 app.prepare().then(() => {
   const server = new GraphQLServer({
     resolvers,
-    typeDefs: './server/schema.graphql',
+    typeDefs: './server/graphql/schema.gql',
     context: ({ request }) => ({ req: request, prisma })
   })
 
-  server.express.use(router(app))
+  server.express.use(routes(app))
 
   const options = {
     port: parseInt(process.env.PORT, 10) || 3000,
