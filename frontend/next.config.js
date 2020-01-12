7adb7cf2be4c5c2f9b6664b440594de46eab27d3
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2020-01-12 10:43:13 -0500

    chore: add log to debug API_URL on next build process

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,5 +1,10 @@
 require('dotenv').config()
 
+console.log(
+  '[frontend:next.config.js]: process.env.API_URL=',
+  process.env.API_URL
+)
+
 module.exports = {
   env: {
     API_URL: process.env.API_URL
