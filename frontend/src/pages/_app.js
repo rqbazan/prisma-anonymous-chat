33e4b43e65054c601073f3f286ddde36533ae95e
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2020-01-12 18:07:40 -0500

    feat: add zeit-toast-clone 😍

--- frontend/src/pages/_app.js
+++ frontend/src/pages/_app.js
@@ -1,8 +1,17 @@
 import React from 'react'
 import NextApp from 'next/app'
+import dynamic from 'next/dynamic'
 import { ThemeProvider } from '@xstyled/styled-components'
 import theme from '~/theme'
 import Preflight from '~/preflight'
+import 'zeit-toast-clone/styles.css'
+
+const NotifierPortal = dynamic(
+  () => import('zeit-toast-clone').then(mod => mod.NotifierPortal),
+  {
+    ssr: false
+  }
+)
 
 export default class App extends NextApp {
   render() {
@@ -11,6 +20,7 @@ export default class App extends NextApp {
     return (
       <ThemeProvider theme={theme}>
         <Preflight />
+        <NotifierPortal />
         <Component {...pageProps} />
       </ThemeProvider>
     )
--- frontend/src/widgets/update-user-info-modal/index.js
+++ frontend/src/widgets/update-user-info-modal/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useMutation } from '@apollo/react-hooks'
 import { Box } from '@xstyled/styled-components'
 import { useForm } from 'react-hook-form'
+import notifier from 'zeit-toast-clone'
 import updateUserMutation from '~/graphql/update-user'
 import FieldInput from '~/components/field-input'
 import Button from '~/components/button'
@@ -17,6 +18,7 @@ export default function UpdateUserInfoModal({ user, controller }) {
     try {
       await updateUser({ variables: values })
       controller.close()
+      notifier.success('Update successful 🎉')
     } catch (error) {
       // eslint-disable-next-line
       error.graphQLErrors?.map(({ message }) => {
