4ff1172b0d66c58e5b0742de25052688eefd88f4
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2020-01-12 18:44:40 -0500

    feat: add border-color to button:focus

--- frontend/src/components/button/elements.js
+++ frontend/src/components/button/elements.js
@@ -1,18 +1,24 @@
 import styled from '@xstyled/styled-components'
 
 export const ButtonAtom = styled.button`
-  display: flex;
   align-items: center;
-  justify-content: center;
-  position: relative;
   background-color: gray.7;
+  border-color: transparent;
+  border-radius: default;
+  border-style: solid;
+  border-width: 2;
   color: light;
+  display: flex;
   font-size: 1;
   font-weight: medium;
+  justify-content: center;
   padding: 2 3;
-  border-radius: default;
+  position: relative;
 
   &:focus {
+    border-color: indigo.6;
+    border-style: solid;
+    border-width: 2;
     outline: none;
   }
 
