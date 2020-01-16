4903b2d1a4ef9f0042cb88383e8e7550fb8d75d6
Ricardo Alexis Quiroz Bazan <raquirozbazan@gmail.com>
2020-01-16 13:15:17 -0500

    build: upgrade nextjs to v9.2.0

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,13 +1,12 @@
 require('dotenv').config()
-const withCSS = require('@zeit/next-css')
 
 console.log(
   `[frontend:next.config.js]: process.env.SERVER_URL=${process.env.SERVER_URL}`
 )
 
-module.exports = withCSS({
+module.exports = {
   useFileSystemPublicRoutes: false,
   env: {
     SERVER_URL: process.env.SERVER_URL
   }
-})
+}
